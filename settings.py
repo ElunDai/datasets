2276b435fff65dc3d8c74ce9f32a02a9d8fc31c5
ElunDai <daielun@outlook.com>
2018-06-30 15:17:37 +0800

    change settings

--- settings.py
+++ settings.py
@@ -3,11 +3,11 @@
 # -*- coding: utf-8 -*-
 #==============================
 #    Author: Elun Dai
-#    Last modified: 2018-06-29 14:55
+#    Last modified: 2018-06-29 14:56
 #    Filename: settings.py
 #    Description:
 #    
 #=============================#
 
-BASE_DIRECTORY = '/tmp/datasets/'
-# BASE_DIRECTORY = '~/my/packages/python/datasets/data/'
+# BASE_DIRECTORY = '/tmp/datasets/'
+BASE_DIRECTORY = '~/my/packages/python/datasets/data/'
